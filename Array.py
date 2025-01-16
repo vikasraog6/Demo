4091ddd5a08a93b7a3bd4cdb7fcfc00bfe54c5a7
vikasraog6 <vikasraog6@gmail.com>
2025-01-16 14:19:10 -0600

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,17 @@
+def find_duplicates(arr):
+    seen = set()
+    duplicates = set()
+
+    for num in arr:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+
+    return list(duplicates)
+
+
+numbers = [1, 2, 3, 4, 5, 2, 6, 3, 7, 8, 8]
+duplicates = find_duplicates(numbers)
+print("Duplicates:", duplicates)
+print (1,2,3,)
